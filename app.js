6ddeaf7efd3f57ed2066007d6adda795a1fbf687
code_max <ogbujigodwinmax@gmail.com>
2022-02-02 14:22:59 +0100

    used cloud database

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 
 const globalErrorHandler = require("./controllers/error");
-const userRouter = require("./routes/userRoute");
+const userRouter = require("./routes/user");
 
 const app = express();
 
